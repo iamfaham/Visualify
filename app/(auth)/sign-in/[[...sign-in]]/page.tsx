d027dfcf898b78360fe784a3fdce43face00023b
Faham <129623271+iamfaham@users.noreply.github.com>
2024-08-03 20:03:54 +0530

    Add color to signup and signin

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,19 @@ import React from 'react'
 
 const SignInPage = () => {
   return (
-    <SignIn path="/sign-in" />
+    <SignIn path="/sign-in" 
+      appearance={{
+      elements: {
+        formButtonPrimary: {
+          backgroundColor: '#5cbefb', // Cyan color
+          borderColor: '#5cbefb',     // Cyan color
+          color: '#ffffff',           // White text color
+        },
+        footerActionLink: {
+          color:'#5cbefb',
+        },
+      },
+    }}/>
   )
 }
 
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,20 @@ import React from 'react'
 
 const SignUpPage = () => {
   return (
-    <SignUp path="/sign-up" />
+    <SignUp path="/sign-up" 
+      appearance={{
+      elements: {
+        formButtonPrimary: {
+          backgroundColor: '#5cbefb', // Cyan color
+          borderColor: '#5cbefb',     // Cyan color
+          color: '#ffffff',           // White text color
+        },
+        footerActionLink: {
+          color:'#5cbefb',
+        },
+      },
+    }}
+    />
   )
 }
 
